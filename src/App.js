c666b88add5f5ebe002b5b1b62c02bf59d9f5289
albertoaguilar8 <albertoaguilar8@gmail.com>
2025-03-28 15:43:25 +0000

    actualizar

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { Tarjeta } from './Tarjeta'
 export function App() {
   return (
     <div>
-        <div>Personajes de Mario Bros</div>
-        <Tarjeta/>
-        <Tarjeta/>
-        <Tarjeta/>
+        <div>Personajes de Mario Bros que mas me gustan</div>
+        <Tarjeta nombre="Mario" colorFavorito="Rojo"/>
+        <Tarjeta nombre="Luigi" colorFavorito="Verde"/>
+        <Tarjeta nombre="Peaches" colorFavorito="Rosa"/>
     </div>
   )
 }
